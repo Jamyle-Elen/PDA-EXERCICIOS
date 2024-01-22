9048bded34e63a39d33d17f0a9686c974d40ea6f
Jamyle Elen Cardoso do Nascimento <110051309+Jamyle-Elen@users.noreply.github.com>
2024-01-22 20:36:48 -0300

    Atividade - Número suspeito

---
+++ Janeiro/22-01-24/script.js
@@ -0,0 +1,24 @@
+const fala1 = "Não sou eu!!";
+const fala2 = "Eu tentei te ajudar desde o início!!";
+const fala3 = "Já pensou que o monstro pode ser controlado?";
+
+function contarLetras(fala) {
+  const falaSemEspacos = fala.replace(/\s/g, '');
+  return falaSemEspacos.length;
+}
+
+const letrasFala1 = contarLetras(fala1);
+const letrasFala2 = contarLetras(fala2);
+const letrasFala3 = contarLetras(fala3);
+//  okay
+let numeroSuspeito;
+
+if (letrasFala1 > letrasFala2 && letrasFala1 > letrasFala3) {
+  numeroSuspeito = 1;
+} else if (letrasFala2 > letrasFala1 && letrasFala2 > letrasFala3) {
+  numeroSuspeito = 2;
+} else {
+  numeroSuspeito = 3;
+}
+
+console.log(`O suspeito com a maior quantidade de letras é o número ${numeroSuspeito}.`);
