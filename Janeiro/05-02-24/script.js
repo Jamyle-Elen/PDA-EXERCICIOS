ee0e976bc4c59866341eeab4bc2eefd95d3a5566
Jamyle <110051309+Jamyle-Elen@users.noreply.github.com>
2024-02-05 20:56:45 -0300

    atividade

---
+++ Janeiro/05-02-24/script.js
@@ -0,0 +1,17 @@
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
+
+let i = 1;
+while (i <= 10) {
+  console.log(i);
+  i++;
+}
+
+// VANTAGENS
+// flexibilidade no codigo
+// simples alteração
+
+// DESVANTAGENS
+// while seria mais longo e requer uma inicialização manual da variavel.
+// por ser mais extenso pode causar acidentalmente um loop infinito.
